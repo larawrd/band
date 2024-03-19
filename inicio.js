56ff000eca5f66c0943c6cc592147a5fbc81a240
Aitana <aitanadelreyl@gmail.com>
2024-03-19 19:00:20 +0100

    second commit

---
+++ inicio.js
@@ -0,0 +1,8 @@
+// import Atropos library
+import Atropos from 'atropos';
+
+// Initialize
+document.addEventListener('DOMContentLoaded', function() {
+    var atropos = Atropos.create('.my-atropos'); // Utiliza el selector '.my-atropos'
+  });
+  
