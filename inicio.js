c202cb60904faff8dbebc74157920115e6035484
larawrd <aitanadelreyl@gmail.com>
2024-03-25 10:37:39 +0100

    era necesario

--- inicio.js
+++
@@ -1,8 +0,0 @@
-// import Atropos library
-import Atropos from 'atropos';
-
-// Initialize
-document.addEventListener('DOMContentLoaded', function() {
-    var atropos = Atropos.create('.my-atropos'); // Utiliza el selector '.my-atropos'
-  });
-  
